1f1d1c92fcda4fb324ce0873fff9cc9a9b9037b7
Diana Badas <diana.badas@gmail.com>
2019-01-07 15:32:15 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -80,7 +80,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, data|
     puts "#{season.to_s.capitalize}:"
     data.each do |attribute, value|
-      puts "#{attribute.to_s.split('_').collect {|word| word.capitalize }.join(' ') }: #{value.join(", ")}"
+      puts " #{attribute.to_s.split('_').collect {|word| word.capitalize }.join(' ') }: #{value.join(", ")}"
     end
   end
 end
