283a4ecd5e752fc5afc7fb5722e88a957f12e6bb
Diana Badas <diana.badas@gmail.com>
2019-01-07 15:23:22 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -90,7 +90,9 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
   holiday_hash.each do |season, data|
     data.each do |attribute, value|
-
+      if value.inlcude?("BBQ")
+        
+      end 
     end
   end
 
